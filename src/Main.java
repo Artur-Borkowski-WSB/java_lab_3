2f8ef05c0761cadd8ea0242e930a59d1b28c3764
Artur Borkowski <gdx149021@student.wsb.gda.pl>
2022-01-16 13:01:22 +0100

    cw3

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,37 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        /* ćw 3 klasy */
+		/* ćw 3 klasy */
 
-        /*  zad.1 - Najpierw należy uzupełnić kod w klasie Punkt.java,
-            a) stworzyć objekt punktA typu Punkt,
-            w konstruktorze zainicjować zmienną pX --> np. new Punkt(7), wykonać metodę punktA.toString();
-            b) następnie ponownie wywopać konstruktor inicjalizujący wszystkie pola z klasy
-            --> np. punkt = new Punkt(1, 2, 3);, wykonać metodę punktA.toString();
-            c) nadpisać wartości pól z objektu punktA za pomocą setterów i
-            ponownie wywołać metodę toString();
-            d) wypisać wartości pól (za pomocą getterów),
-            e) wywołać metody suma i róznica (obie wersje) z klasy punkt
-        */
-
-    }
+    /*  zad.1 - Najpierw należy uzupełnić kod w klasie Punkt.java,
+        a) stworzyć objekt punktA typu Punkt,
+        w konstruktorze zainicjować zmienną pX --> np. new Punkt(7), wykonać metodę punktA.toString();
+        b) następnie ponownie wywopać konstruktor inicjalizujący wszystkie pola z klasy
+        --> np. punkt = new Punkt(1, 2, 3);, wykonać metodę punktA.toString();
+        c) nadpisać wartości pól z objektu punktA za pomocą setterów i
+        ponownie wywołać metodę toString();
+        d) wypisać wartości pól (za pomocą getterów),
+        e) wywołać metody suma i róznica (obie wersje) z klasy punkt
+    */
+		Punkt punktA = new Punkt(7);
+		System.out.println(punktA); // niejawny toString
+		punktA = new Punkt(1, 2, 3);
+		System.out.println(punktA); // niejawny toString
+		punktA.setpX(100);
+		punktA.setpY(25);
+		punktA.setpZ(10);
+		System.out.println(punktA); // niejawny toString
+		System.out.println("Get XYZ");
+		System.out.println(punktA.getpX());
+		System.out.println(punktA.getpY());
+		System.out.println(punktA.getpZ());
+		System.out.println("Suma");
+		System.out.println(punktA.suma());
+		System.out.println("Różnica");
+		System.out.println(punktA.roznica());
+		System.out.println("Różnica V2");
+		System.out.println(punktA.roznica(2, 4, 10));
+	}
 }
--- src/Punkt.java
+++ src/Punkt.java
@@ -1,23 +1,82 @@
 /* klasa Punkt */
 public class Punkt {
-    /* stworzyć zmienne prywatne pX, pY, pZ */
+	/* stworzyć zmienne prywatne pX, pY, pZ */
+	private int pX, pY, pZ;
 
-    /* stworzyć
-        a) pusty konstruktor,
-        b) konstruktor inicjalizujacy zmienną pX,
-        c) konstruktor inicjalizujący wszystkie zmienne,
-        d) stworzyć settery i gettery,
-        e) getter do zmiennej pZ niech zwraca return pZ * 10;
+	/* stworzyć
+		a) pusty konstruktor,
+		b) konstruktor inicjalizujacy zmienną pX,
+		c) konstruktor inicjalizujący wszystkie zmienne,
+		d) stworzyć settery i gettery,
+		e) getter do zmiennej pZ niech zwraca return pZ * 10;
 
-        podpowiedz ALT + INSERT można wybrać generowanie automatyczne
-        konstruktora, getterów, setterów, nadpisywanie metody toString() i inne ...
-    */
+		podpowiedz ALT + INSERT można wybrać generowanie automatyczne
+		konstruktora, getterów, setterów, nadpisywanie metody toString() i inne ...
+	*/
 
-    /* Stworzyć metody
-        a) suma - sumująca wszystkie wartości w klasie
-        b) różnica - metody przeciążone roznica(){} i roznica(int x, int y, int z){}
-        c) metody różnica mają wykonwywać dzialania :
-            roznica(){ return pX - pY - pZ} i roznica(int x, int y, int z){ return pX*x - pY*y - pZ*z }
-        d) nadpisać metodę toString(), aby zwracała wszystkie wartości pól, analogicznie jak w przykładzie
-     */
+	public Punkt() {
+	}
+
+	public Punkt(int pX) {
+		this.pX = pX;
+	}
+
+	public Punkt(int pX, int pY, int pZ) {
+		this.pX = pX;
+		this.pY = pY;
+		this.pZ = pZ;
+	}
+
+	public int getpX() {
+		return pX;
+	}
+
+	public void setpX(int pX) {
+		this.pX = pX;
+	}
+
+	public int getpY() {
+		return pY;
+	}
+
+	public void setpY(int pY) {
+		this.pY = pY;
+	}
+
+	public int getpZ() {
+		return pZ * 10;
+	}
+
+	public void setpZ(int pZ) {
+		this.pZ = pZ;
+	}
+
+	/* Stworzyć metody
+		a) suma - sumująca wszystkie wartości w klasie
+		b) różnica - metody przeciążone roznica(){} i roznica(int x, int y, int z){}
+		c) metody różnica mają wykonwywać dzialania :
+				roznica(){ return pX - pY - pZ} i roznica(int x, int y, int z){ return pX*x - pY*y - pZ*z }
+		d) nadpisać metodę toString(), aby zwracała wszystkie wartości pól, analogicznie jak w przykładzie
+ 	*/
+
+	int suma() {
+		return this.pX + this.pY + this.pZ;
+	}
+
+	int roznica() {
+		return this.pX - this.pY - this.pZ;
+	}
+
+	int roznica(int x, int y, int z) {
+		return this.pX * x - this.pY * y - this.pZ * z;
+	}
+
+	@Override
+	public String toString() {
+		return "Punkt{" +
+			"pX=" + pX +
+			", pY=" + pY +
+			", pZ=" + pZ +
+			'}';
+	}
 }
